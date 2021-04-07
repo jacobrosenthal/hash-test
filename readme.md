2522c6df7c9623f6cfdf9eb77972a878fc6827ec
Jacob Rosenthal <jacobrosenthal@gmail.com>
2021-04-07 15:07:01 -0700

    sponge

---
+++ readme.md
@@ -0,0 +1,14 @@
+# hash-test
+
+`npx webpack --config webpack.config.js && python3 -m http.server 8000 --directory dist`
+
+Brave:
+```
+loading
+main.js:1 Call to sponge average 3.961621000001105 milliseconds.
+main.js:1 
+
+
+main.js:1 Call to js 0.0007714999974268721 milliseconds.
+main.js:1 
+```
--- src/index.js
+++ src/index.js
@@ -1,44 +1,7 @@
-import { default as wasm, mimc } from "mimc-wasm";
+import { default as wasm, sponge } from "mimc-wasm";
 import mimcWithRounds from './mimc.js';
 
 await wasm();
-
-
-const LOCATION_ID_UB = BigInt(
-    '21888242871839275222246405745257275088548364400416034343698204186575808495617'
-);
-
-
-const locationIdFromBigInt = (location) => {
-    const locationBI = BigInt(location);
-    if (locationBI > LOCATION_ID_UB) throw new Error('not a valid location');
-    let ret = locationBI.toString(16);
-    while (ret.length < 64) ret = '0' + ret;
-    return ret;
-};
-
-const mimcjs = (task) => {
-    let planetLocations = [];
-    const planetRarityBI = BigInt(task.planetRarity);
-    const { x: bottomLeftX, y: bottomLeftY } = task.chunkFootprint.bottomLeft;
-    const { sideLength } = task.chunkFootprint;
-    for (let x = bottomLeftX; x < bottomLeftX + sideLength; x++) {
-        for (let y = bottomLeftY; y < bottomLeftY + sideLength; y++) {
-            const hash = mimcWithRounds(220, task.planetHashKey, x, y);
-            if (hash < (LOCATION_ID_UB / planetRarityBI)) {
-                planetLocations.push({
-                    coords: { x, y },
-                    hash: locationIdFromBigInt(hash),
-                });
-            }
-        }
-    }
-    return planetLocations;
-}
-
-
-
-
 
 
 
@@ -48,7 +11,7 @@ let task = {
             x: 0,
             y: 0,
         },
-        sideLength: 256
+        sideLength: 16
     },
     planetHashKey: 8,
     planetRarity: 16384
@@ -56,20 +19,30 @@ let task = {
 
 console.log("loading");
 
-var t0 = performance.now()
-var blah = mimc(task);
-console.log(blah);
-var t1 = performance.now()
-var time = (t1 - t0);
-console.log("Call to mimc " + time + " milliseconds.")
+var times = [];
+for (let i = 0; i < 10000; i++) {
+    var t0 = performance.now()
+    var hash = sponge(task.chunkFootprint.bottomLeft.x, task.chunkFootprint.bottomLeft.y, 220, task.planetHashKey);
+    var bigHash = BigInt(hash);
+    var t1 = performance.now()
+    var time = (t1 - t0);
+    times.push(time);
+}
+var avg = times.reduce((accumulator, currentValue) => accumulator + currentValue) / times.length;
+console.log("Call to sponge average " + avg + " milliseconds.")
 console.log('\n\n')
 
-var t0 = performance.now()
-var blah = mimcjs(task);
-console.log(blah);
-var t1 = performance.now()
-var time = (t1 - t0);
-console.log("Call to js " + time + " milliseconds.")
+
+var times = [];
+for (let i = 0; i < 10000; i++) {
+    var t0 = performance.now()
+    var hash = mimcWithRounds(220, task.planetHashKey, task.chunkFootprint.bottomLeft.x, task.chunkFootprint.bottomLeft.y);
+    var t1 = performance.now()
+    var time = (t1 - t0);
+    times.push(time);
+}
+var avg = times.reduce((accumulator, currentValue) => accumulator + currentValue) / times.length;
+console.log("Call to js " + avg + " milliseconds.")
 console.log('\n\n')
 
 
